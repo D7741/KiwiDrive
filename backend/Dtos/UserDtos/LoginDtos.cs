c749aa7836b30243ba19fc59fa0cef5c86a27515
D7741 <149285047+D7741@users.noreply.github.com>
2026-07-06 22:10:36 +1200

    fix: rename DTOs to Dtos

---
+++ backend/Dtos/UserDtos/LoginDtos.cs
@@ -0,0 +1,9 @@
+namespace KiwiDrive.DTOs
+{
+    public class LoginDtos
+    {
+        public string Email {get; set;} = string.Empty;
+
+        public string PasswordHash {get; set;} = string.Empty;
+    }
+}
