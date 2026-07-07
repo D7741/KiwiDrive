9235724793010814ca56fb63fb679db231314a05
D7741 <149285047+D7741@users.noreply.github.com>
2026-07-07 21:54:23 +1200

    backend/Dtos/QuestionDtos/AnswerSubmitDto.cs

---
+++ backend/Dtos/QuestionDtos/AnswerSubmitDto.cs
@@ -0,0 +1,10 @@
+namespace KiwiDrive.Dtos.QuestionDtos
+{
+    public class AnswerSubmitDto
+    {
+        public int QuestionId {get; set;}
+
+        public string Answer {get; set;} = string.Empty;
+
+    }
+}
