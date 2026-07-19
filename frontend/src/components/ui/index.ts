7414c5c8cf673d5649c9f23baa34c0eccdb71949
D7741 <149285047+D7741@users.noreply.github.com>
2026-07-19 23:27:32 +1200

    feat: add component portal for pages

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,9 @@
+// src/components/ui/index.ts
+export { default as Button } from './Button'
+export { default as Badge } from './Badge'
+export { default as Card } from './Card'
+export { default as Input } from './Input'
+export { default as NavBar } from './NavBar'
+export { default as QuestionCard } from './QuestionCard'
+export { default as XPBar } from './XPBar'
+export { default as StreakFlame } from './StreakFlame'
