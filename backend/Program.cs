f7b47ccf5418847d77012fda8b54d35b9daea6a8
D7741 <149285047+D7741@users.noreply.github.com>
2026-07-25 22:25:48 +1200

    chore(program.cs): fetch frontend public link to backend.

--- backend/Program.cs
+++ backend/Program.cs
@@ -29,7 +29,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins(
+            "http://localhost:5173",
+            "https://happy-dune-06daefb00.7.azurestaticapps.net"
+            )
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
